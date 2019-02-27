295e53972d532433f73f0e8e4ef536d9b81693f5
w3bgir1 <akulshyna@gmail.com>
2019-02-27 11:00:09 +0100

    list added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import DogsListContainer from './components/DogsListContainer'
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
+        <header className="App-header"> 
         </header>
+        <main>
+          <DogsListContainer />
+        </main>
       </div>
     );
   }
